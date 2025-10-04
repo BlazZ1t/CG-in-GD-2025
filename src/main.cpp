f2c539f31777e7c4dd2a95ec02e5da748e7ed861
BlazZ1t <klimspy@gmail.com>
2025-10-04 09:04:05 +0300

    Finished lab 1.02

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "renderer/renderer.h"
 #include "settings.h"
+#include "chrono"
+#include "stdio.h"
 
 #include <iostream>
 
@@ -10,11 +12,16 @@ int main(int argc, char** argv)
 		auto settings = cg::settings::parse_settings(argc, argv);
 		auto renderer = cg::renderer::make_renderer(settings);
 
+		auto start = std::chrono::high_resolution_clock::now();
 		renderer->init();
 
 		renderer->render();
 
 		renderer->destroy();
+
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		std::cout << duration.count() << "ms" << std::endl;
 	}
 	catch (std::exception& e)
 	{
