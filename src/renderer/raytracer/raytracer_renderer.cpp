e9dbf75a496a4e9aa8efd57bb9d382922c33dfd1
BlazZ1t <klimspy@gmail.com>
2025-11-03 20:43:23 +0300

    Finish Lab 2.06 and monte-carlo

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -46,11 +46,17 @@ void cg::renderer::ray_tracing_renderer::render()
 	raytracer->miss_shader = [](const ray& ray) 
 	{
 		payload payload{};
+		payload.color = {0.f, 0.f, 0.f};
 
-		payload.color = {0.f, 0.f, (ray.direction.y + 1.f) * 0.5f};
+		// payload.color = {0.f, 0.f, (ray.direction.y + 1.f) * 0.5f};
 
 		return payload;
 	};
+
+	std::random_device random_device;
+	std::mt19937 random_generator(random_device());
+	std::uniform_real_distribution<float> uniform(-1.f, 1.f);
+
 
 	raytracer->closest_hit_shader = [&](const ray& ray, payload& payload,
 										const triangle<cg::vertex>& triangle, size_t depth) {
@@ -63,17 +69,19 @@ void cg::renderer::ray_tracing_renderer::render()
 		);
 
 		float3 result_color = triangle.emissive;
+		float3 random_direction(uniform(random_generator), uniform(random_generator), uniform(random_generator));
 
-		for (auto & light : lights)
+		if (dot(normal, random_direction) < 0.f)
 		{
-			cg::renderer::ray to_light(position, light.position - position);
-			auto shadow_payload = shadow_raytracer->trace_ray(to_light, 1, length(light.position - position));
-
-			if (shadow_payload.t < 0.f) {
-				result_color += triangle.diffuse * light.color * std::max(dot(normal, to_light.direction), 0.f);
-			}
-			
+			random_direction = -random_direction;
 		}
+
+		
+		cg::renderer::ray to_next_object(position, random_direction);
+		auto next_payload = raytracer->trace_ray(to_next_object, depth);
+
+
+		result_color += triangle.diffuse * next_payload.color.to_float3() * std::max(dot(normal, to_next_object.direction), 0.f);
 
 		payload.color = cg::color::from_float3(result_color);
 		return payload;
@@ -91,7 +99,7 @@ void cg::renderer::ray_tracing_renderer::render()
 		return payload;
 	};
 
-	shadow_raytracer->build_acceleration_structure();
+	shadow_raytracer->acceleration_structures = raytracer->acceleration_structures;
 
 	{
 		cg::utils::timer t("Ray generation");
@@ -110,6 +118,4 @@ void cg::renderer::ray_tracing_renderer::render()
 		
 		cg::utils::save_resource(*render_target, settings->result_path);
 	}
-	// TODO Lab: 2.05 Adjust `ray_tracing_renderer` class to build the acceleration structure
-	// TODO Lab: 2.06 (Bonus) Adjust `closest_hit_shader` for Monte-Carlo light tracing
 }
